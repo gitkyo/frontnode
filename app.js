2caa5a40a161c74eaa2535e15ae636c10e0dbb3b
Pier <p.delaunay1@gmail.com>
2022-01-26 11:16:56 +0400

    ajout de eslint en mode de

--- app.js
+++ app.js
@@ -1,55 +1,54 @@
-import express from 'express'
-import pkg from 'hbs';
-const { hbs } = pkg;
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path'
+import express from "express";
+import pkg from "hbs";
+// const { hbs } = pkg;
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
 
 
 //set var to get path directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const publishDirectoryPath = path.join(__dirname, '/public')
-const viewsPath = path.join(__dirname, '/templates/views')
-const partialsPath = path.join(__dirname, 'templates/partials')
+const publishDirectoryPath = path.join(__dirname, "/public");
+const viewsPath = path.join(__dirname, "/templates/views");
+const partialsPath = path.join(__dirname, "templates/partials");
 
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = 3000;
 
 //on indique qu'on utilise le format hbs comme template engine
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 //redefinition du repertoire des vues
-app.set('views', viewsPath)
+app.set("views", viewsPath);
 
 //definition du repertoire des partials https://handlebarsjs.com/guide/partials.html#basic-partials
-pkg.registerPartials(partialsPath)
+pkg.registerPartials(partialsPath);
 
 
 //on indique le repertoire public pour les fichiers statiques
-app.use(express.static(publishDirectoryPath))
+app.use(express.static(publishDirectoryPath));
 
 //création d'une route pour la page index
-app.get('/', (req, res)=>{
-    res.render('index', {
-        title : 'Starter front NodeJS',        
-    })
-})
+app.get("/", (req, res)=>{
+    res.render("index", {
+        title : "Starter front NodeJS",        
+    });
+});
 
 //creation d'une route pour la page about
-app.get('/about', (req, res)=>{
-    res.render('about', {
-        title : 'page about',        
-    })
-})
-
+app.get("/about", (req, res)=>{
+    res.render("about", {
+        title : "page about",        
+    });
+});
 
 
 //demarrare de notre serveur
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
 
 
-  // todo montrer le templating + integration carte + heroku
+// todo montrer le templating + integration carte + heroku
